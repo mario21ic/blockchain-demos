1d0da08bb2748db7c65c6370a9a540e7fe2be78f
Mario IC <mario21ic@gmail.com>
2022-04-02 23:06:09 -0500

    feat(global) adding geth recipe

---
+++ geth/README.md
@@ -0,0 +1,1 @@
+Based on https://chainskills.com/2017/02/24/create-a-private-ethereum-blockchain-with-iot-devices-16/
---
+++ geth/private_start.sh
@@ -0,0 +1,3 @@
+geth --identity "miner1" --networkid 42 --datadir "~/ChainSkills/miner1" --nodiscover --mine --port "30303" --unlock 0 --password ~/ChainSkills/miner1/password.sec --ipcpath "~/.ethereum/geth.ipc"
+
+
